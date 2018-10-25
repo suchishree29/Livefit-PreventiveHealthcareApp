0fbd3f84e28c9228e70b1262414e6d2e69abf078
Suchishree Jena <suchishree.jena@ibm.com>
2018-10-24 22:34:16 -0700

    Added DatePicker to Log Food

--- Health Application/app/src/main/java/com/example/c02hp1dtdv35/healthapplication/BarcodeScanner/LogFood.java
+++ Health Application/app/src/main/java/com/example/c02hp1dtdv35/healthapplication/BarcodeScanner/LogFood.java
@@ -1,12 +1,17 @@
 package com.example.c02hp1dtdv35.healthapplication.BarcodeScanner;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,12 +19,17 @@ import com.example.c02hp1dtdv35.healthapplication.R;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
+import java.util.Calendar;
+
 public class LogFood extends AppCompatActivity {
 
     private ImageView prodImg;
     private TextView prodName;
     private TextView nutritionFacts;
+    private TextView dateTxt;
     Nutriments nutriments;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private static final String TAG = "LogFood";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +41,11 @@ public class LogFood extends AppCompatActivity {
         this.prodName.setMovementMethod(new ScrollingMovementMethod());
         nutritionFacts = findViewById(R.id.nutrition_facts);
         nutritionFacts.setMovementMethod(LinkMovementMethod.getInstance());
-//        String text = "<a href='http://www.google.com'> NUTRITION FACTS </a>";
-//        nutritionFacts.setText(Html.fromHtml(text));
 
         //Get the bundle
         Bundle bundle = getIntent().getExtras();
 
-        //Extract the data…
+        //Extract the product name and image…
         String product = bundle.getString("product_name");
         this.prodName.setText("Product Name: " + product);
         String imgUrl = bundle.getString("product_image");
@@ -51,7 +59,35 @@ public class LogFood extends AppCompatActivity {
         }
         nutriments = new Gson().fromJson(jsonMyObject, Nutriments.class);
 
+        // Code for Date Picker
+        dateTxt = findViewById(R.id.dayTxt);
+        dateTxt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
 
+                DatePickerDialog dialog = new DatePickerDialog(LogFood.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day
+                );
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG,"onDateSet: Day: "+ month +"/" + day + "/" + year);
+
+                String date = month +"/" + day + "/" + year;
+                dateTxt.setText("Day : " + date);
+            }
+        };
 
     }
 
