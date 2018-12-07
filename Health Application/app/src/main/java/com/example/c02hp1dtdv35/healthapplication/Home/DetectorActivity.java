1af849208e55bf02bc3e141c05a35a48570b802d
SadabQ <shadab.qureshi88@gmail.com>
2018-12-07 02:44:43 -0800

    Comment removed

--- Health Application/app/src/main/java/com/example/c02hp1dtdv35/healthapplication/Home/DetectorActivity.java
+++ Health Application/app/src/main/java/com/example/c02hp1dtdv35/healthapplication/Home/DetectorActivity.java
@@ -57,50 +57,20 @@ import com.example.c02hp1dtdv35.healthapplication.R; // Explicit import needed f
 import com.example.c02hp1dtdv35.healthapplication.TensorFlowObjectDetectionAPIModel;
 import com.google.gson.Gson;
 
-/**
- * An activity that uses a TensorFlowMultiBoxDetector and ObjectTracker to detect and then track
- * objects.
- */
+
 public class DetectorActivity extends CameraActivity implements OnImageAvailableListener {
   private static final Logger LOGGER = new Logger();
 
-  // Configuration values for the prepackaged multibox model.
-  private static final int MB_INPUT_SIZE = 224;
-  private static final int MB_IMAGE_MEAN = 128;
-  private static final float MB_IMAGE_STD = 128;
-  private static final String MB_INPUT_NAME = "ResizeBilinear";
-  private static final String MB_OUTPUT_LOCATIONS_NAME = "output_locations/Reshape";
-  private static final String MB_OUTPUT_SCORES_NAME = "output_scores/Reshape";
-  private static final String MB_MODEL_FILE = "file:///android_asset/multibox_model.pb";
-  private static final String MB_LOCATION_FILE =
-          "file:///android_asset/multibox_location_priors.txt";
 
   private static final int TF_OD_API_INPUT_SIZE = 300;
     private static final String TF_OD_API_MODEL_FILE = "file:///android_asset/food_inference_graph.pb";
-//    private static final String TF_OD_API_MODEL_FILE = "file:///android_asset/ssd_mobilenet_v1_android_export.pb";
-//  private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/coco_labels_list.txt";
     private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/livefit_labels_list.txt";
 
-  // Configuration values for tiny-yolo-voc. Note that the graph is not included with TensorFlow and
-  // must be manually placed in the assets/ directory by the user.
-  // Graphs and models downloaded from http://pjreddie.com/darknet/yolo/ may be converted e.g. via
-  // DarkFlow (https://github.com/thtrieu/darkflow). Sample command:
-  // ./flow --model cfg/tiny-yolo-voc.cfg --load bin/tiny-yolo-voc.weights --savepb --verbalise
-  private static final String YOLO_MODEL_FILE = "file:///android_asset/graph-tiny-yolo-voc.pb";
-  private static final int YOLO_INPUT_SIZE = 416;
-  private static final String YOLO_INPUT_NAME = "input";
-  private static final String YOLO_OUTPUT_NAMES = "output";
-  private static final int YOLO_BLOCK_SIZE = 32;
-
-  // Which detection model to use: by default uses Tensorflow Object Detection API frozen
-  // checkpoints.  Optionally use legacy Multibox (trained using an older version of the API)
-  // or YOLO.
   private enum DetectorMode {
     TF_OD_API, MULTIBOX, YOLO;
   }
   private static final DetectorMode MODE = DetectorMode.TF_OD_API;
 
-  // Minimum detection confidence to track a detection.
   private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.8f;
   private static final float MINIMUM_CONFIDENCE_MULTIBOX = 0.1f;
   private static final float MINIMUM_CONFIDENCE_YOLO = 0.25f;
@@ -157,11 +127,6 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
         toast.show();
         finish();
       }
-
-//    Application application = (Application) getApplication();
-//
-//    detector = application.getDetector();
-//    cropSize = TF_OD_API_INPUT_SIZE;
 
     previewWidth = size.getWidth();
     previewHeight = size.getHeight();
